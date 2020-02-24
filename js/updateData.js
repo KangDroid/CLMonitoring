be960aa455b54636a401672fc1e3b15aa750e38a
KangDroid <hyunwoo286@gmail.com>
2020-02-24 15:30:17 +0900

    Fallback when cpu_log is not present

--- js/updateData.js
+++ js/updateData.js
@@ -1,4 +1,5 @@
 var fileinputval;
+var boolean;
 
 /**
  * Update each data when first page load
@@ -32,23 +33,38 @@ function updateDetailedNode() {
             cpu_frequency.innerHTML = fileinputval[i] + "Ghz";
         }
     }
-    getText(callbackFunction);
+
+    if (boolean == false) {
+        // do nothing
+        alert("Node Monitoring Service is not running or present.");
+    } else {
+        getText(callbackFunction);
+    }
 }
 
 /**
  * Parse data from Node's Server, show it to HTML!
  * Parsed Data format is: ["1.2", "1.2", "1.2", "1.2", "47.236", "0.49 0.52 0.48 2/174 31638", "", ""]
  */
-function getText(callBack){
+function getText(callBack) {
     var txtFile = new XMLHttpRequest();
     txtFile.open("GET", "http://192.168.0.20/cpu_log", true);
+    // console.log(txtFile.readyState);
+    // console.log(txtFile.status);
     txtFile.onreadystatechange = function() {
         if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
             if (txtFile.status === 200) {  // Makes sure it's found the file.
                 // var allText = txtFile.responseText;
                 // var lines = txtFile.responseText.split("\n"); // Will separate each line into an array
                 fileinputval = txtFile.responseText.split("\n")
+                console.log("Suc..?");
                 callBack();
+            } else if (txtFile.status === 0) {
+                // File Not found or exception, Fallback!
+                if (boolean != false) {
+                    alert("Node Monitoring Service is not running or present.");
+                    boolean = false;
+                }
             }
         }
     }
